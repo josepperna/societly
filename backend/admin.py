48ce6fa48824ece3b59c32cd8311947bc77ae076
Aliou Amar <2310549a@student.gla.ac.uk>
2019-03-04 21:32:54 +0000

    Admin registration

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student, Society, Event, Review, Membership
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Society)
+admin.site.register(Event)
+admin.site.register(Review)
+admin.site.register(Membership)
