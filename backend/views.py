1beb49b571472f57fd3f822f69c00767a2e0e3f8
Francesco Cesana <2297072c@student.gla.ac.uk>
2019-03-17 19:58:53 +0000

    A

--- backend/views.py
+++ backend/views.py
@@ -2,12 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import Student, Society, Event, Review, Membership
+from .models import Student, Society, Event, Review, Membership 
+from .forms import LogInForm
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "societly/index.html") 
+    new_events = Event.objects.order_by('-date')[:10]
+    context_dict = {'new_events' : new_events}
+    return render(request, "societly/home.html",context=context_dict) 
 
+def log_in_form(request):
+	
+	if request.method == 'POST':
+		form = LogInForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('societly/profile/')
+			
+	else:
+			
+		form = LogInForm()
+	return render(request,'societly/LogIn.html',{'form':form})
+
+def profile(request):
+	societies = Society.objects.all()[:3]
+	events = Event.objects.all()[:3]
+	print(societies)
+	print(events)
+	student = request.user.get_username()
+	context_dict =  {'societies':societies,'events':events, 'student' : student}
+	return render(request, "societly/profile.html",context=context_dict) 
+    
 def about_us(request):
     return HttpResponse("Hello again, this is the fucking about us page")
 
